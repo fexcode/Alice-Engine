c299a77ec10422efdc95708261f7084e2554bfef
Mikoris <119640436+xiaomi2023@users.noreply.github.com>
2025-03-08 11:29:48 +0800

    修复python代码调用问题
    修复脚本文件调用python代码无法存储上下文的问题。

--- src/runner/core.py
+++ src/runner/core.py
@@ -10,15 +10,19 @@ def run_ast(ast: NodeTree):
 
 
 def run_node(node: Node):
+    context = {}  # 初始化上下文
+
     # 输出节点文本
     print(node.value)
 
-    # 运行命令
+    # 执行命令并更新上下文
     for cmd in node.cmds:
         if cmd.cmd == "@exit()@":
             exit()
         else:
-            cmd.run()
+            # 执行命令，并将更新后的上下文传回
+            new_context = cmd.run(context)
+            context.update(new_context if new_context else {})
 
     # 打印选项
     for i, opt in enumerate(node.options):
